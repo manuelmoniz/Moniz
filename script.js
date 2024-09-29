2ee84d3c4dd8b592fe9288bdad2f0726010a3911
Manuel Moniz Lukau <lumanuelmoniz@gmail.com>
2024-09-28 23:14:12 -0300

    n

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ if(!isopen){
     duration: 3000,
     close: true,
     gravity: "top", // `top` or `bottom`
-    position: "right", // `left`, `center` or `right`
+    position: "right", // `left`, `center` or `right` 
     stopOnFocus: true, // Prevents dismissing of toast on hover
     style: {
       background: "#ef4444",
